b49e92d2f25db304cc88f2f98c8844c94d62dbe5
Hubert <hubert.razack@weareduotone.com>
2016-09-16 01:00:31 -0400

    Fix side navigation when there are no items

--- happyweb/admin/widgets/navigation/view_full.php
+++ happyweb/admin/widgets/navigation/view_full.php
@@ -18,7 +18,7 @@ if ($sub_pages = $db->get_results("SELECT * FROM page WHERE parent=".$current_pa
   print "</ul>";
 }
 // if not check if it has siblings
-else if ($sibling_pages = $db->get_results("SELECT * FROM page WHERE parent=".$current_page->parent." ORDER BY display_order ASC")) {
+else if ($sibling_pages = $db->get_results("SELECT * FROM page WHERE parent!=0 AND parent=".$current_page->parent." ORDER BY display_order ASC")) {
   print "<ul>";
   foreach($sibling_pages as $p) {
     $class_selected = ($p->id == $current_page->id)?"selected":"";
@@ -26,7 +26,6 @@ else if ($sibling_pages = $db->get_results("SELECT * FROM page WHERE parent=".$c
     <li class="<?php print $class_selected; ?>"><a href="/<?php print $p->url; ?>"><?php print $p->title; ?></a></li>
     <?php
   }
-  print "</ul>";
-  
+  print "</ul>"; 
 }
 ?>
