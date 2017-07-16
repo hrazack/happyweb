52a19d878aebf2f12f2e9af5681d6886e93b4093
Hubert <hubert@livingmelody.com>
2017-07-16 14:32:33 -0400

    Fixed typo in code widget

--- happyweb_system/admin/widgets/code/form_submit.php
+++ happyweb_system/admin/widgets/code/form_submit.php
@@ -5,6 +5,6 @@ if ($action == "create") {
   $db->query("INSERT INTO widget_code (widget_id, filename) VALUES (".$widget_id.", '".$filename."')");
 }
 else {
-  $db->query("UPDATE widget_code SET filename='".$text."' WHERE widget_id=".$widget_id);
+  $db->query("UPDATE widget_code SET filename='".$filename."' WHERE widget_id=".$widget_id);
 }
 ?>
