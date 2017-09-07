95a7a85651a9006fdcacc7fb11fd90a6e934d1f2
Hubert <hubert@livingmelody.com>
2017-09-07 12:08:48 -0400

    Added "What's new" admin page

---
+++ happyweb_system/admin/news.php
@@ -0,0 +1,10 @@
+<?php
+$head_page_title = "What's new";
+$display_navigation = true;
+?>
+
+<p class="help"><i class="material-icons">info_outline</i>Here is a handy list of the latest updates we've done to Happy Web.</p>
+
+<h3>7 September 2017</h3>
+<p>When creating or adding a new page, there is a new option called "hide this page". If you tick this option, the page will not show in the navigation. This can be used while you are working on a page and if you prefer to hide it until it's ready.</p> 
+<p>There is also an option to hide a row on a page. You can click on the "cog" icon on a row, and you will see a new "hide this row" option. This will hide the row on the page. This can also be used if you are working on some content but prefer not to show it until it is finished.</p>
